acca959690df1fb3c6c628ca55340440383cec20
alexxaandr <digBask13Alex37>
2021-11-21 21:05:55 +0300

    rewrite default strapi methods for "words" collection (find, delete, update)

--- backend/api/words/controllers/words.js
+++ backend/api/words/controllers/words.js
@@ -1,8 +1,40 @@
 'use strict';
 
+const {sanitizeEntity} = require("strapi-utils");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: async ctx => {
+    const {id} = ctx.state.user
+
+    const entities = await strapi.services.words.find({
+      _sort: 'created_at:asc',
+      user: id,
+    })
+
+    return entities.map(entity => sanitizeEntity(entity, {model: strapi.models.words}))
+  },
+  delete: async ctx => {
+    const {id} = ctx.state.user
+
+    const wordId = ctx.params.id
+
+    const entity = await strapi.services.words.delete({
+      id: wordId, user: id
+    })
+
+    return sanitizeEntity(entity, {model: strapi.models.words})
+  },
+  update: async ctx => {
+    const { id } = ctx.params
+    const userId = ctx.state.user.id
+
+    const entity = await strapi.services.words.update({id, user: userId}, ctx.request.body)
+
+    return sanitizeEntity(entity, { model: strapi.models.words });
+  },
+};
