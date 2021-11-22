1a575c86fdd5312014096006761590230e0cb89a
alexxaandr <digBask13Alex37>
2021-11-22 23:25:21 +0300

    added the "dictionaries" collection. Rewrite "create" and "delete" default controllers

---
+++ backend/api/dictionaries/controllers/dictionaries.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { sanitizeEntity } = require('strapi-utils');
+
+const sanitizeDictionary = (dictionary) => sanitizeEntity(dictionary, {model: strapi.models.dictionaries})
+
+module.exports = {
+  create: async ctx => {
+    const {id} = ctx.state.user
+
+    const entity = await strapi.services.dictionaries.create({...ctx.request.body, owner: id})
+
+    return sanitizeDictionary(entity)
+  },
+  delete: async ctx => {
+    const {id} = ctx.state.user
+    const dictionaryId = ctx.params.id
+
+    const entity = await strapi.services.dictionaries.delete({id: dictionaryId, owner: id})
+
+    return sanitizeDictionary(entity)
+  }
+};
