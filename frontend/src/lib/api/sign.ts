bbe4d5ae5a4de44eb9b5b5b822f3b53d8c7c0e68
alexandr <alexander.mischenko@bask.ws>
2022-03-09 22:00:56 +0300

    feature/errorHandling error handler has been developed. Toasts message has been added in project. Extra func "request" has been deleted from front api. Error returning from backend for Sign controllers has been developed. Error handler has been integrated in signIn request

--- frontend/src/lib/api/sign.ts
+++ frontend/src/lib/api/sign.ts
@@ -1,6 +1,18 @@
-import {request} from "./useApi";
-import {ISignInPayload} from "../../types/sign";
+import {instance} from "./index";
+import {AxiosError} from 'axios';
+import {errorHandler} from '../utils/helpers/api';
+import {ISignInPayload, ISignUpPayload, IUser} from "../../types/sign";
 
-export const signIn = (data:ISignInPayload) => request('POST', '/sign/signIn', data)
+export const signIn = async (data:ISignInPayload) => {
+    try {
+        return await instance.post<IUser>('/sign/signIn', data)
 
-export const signUp = (data:{}) => request('POST', '/sign/signUp', data)
+    } catch (e) {
+        const error = e as AxiosError
+        errorHandler(error, {
+            400: 'Такого пользователя не существует'
+        })
+    }
+}
+
+export const signUp = (data:ISignUpPayload) => instance.post('/sign/signUp', data)
