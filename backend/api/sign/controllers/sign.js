26e9697a85dfdb03032c69d14010b6234089af27
alexandr <alexander.mischenko@bask.ws>
2022-03-09 22:08:25 +0300

    feature/errorHandling extra statusCode returning in sign controllers has been deleted

--- backend/api/sign/controllers/sign.js
+++ backend/api/sign/controllers/sign.js
@@ -11,7 +11,6 @@ module.exports = {
       const {statusCode} = error.response.data
 
       ctx.send({
-        statusCode,
         data: null,
       }, statusCode)
     }
@@ -27,7 +26,6 @@ module.exports = {
       const {statusCode} = error.response.data
 
       ctx.send({
-        statusCode,
         data: null,
       }, statusCode)
     }
