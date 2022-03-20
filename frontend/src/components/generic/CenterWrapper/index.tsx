fe512511a75a0b58ff06f4651b5ec0a5b6657124
alexandr <alexander.mischenko@bask.ws>
2022-03-20 12:16:34 +0300

    feature/stylingSignPages CenterWrapper has been optimized

--- frontend/src/components/generic/CenterWrapper/index.tsx
+++ frontend/src/components/generic/CenterWrapper/index.tsx
@@ -2,12 +2,11 @@ import {FC} from "react";
 import Grid from "@mui/material/Grid";
 import styles from "./style.module.scss";
 
-const CenterWrapper:FC = ({children}) => {
-    return (
-        <Grid className={styles.wrapper} justifyContent="center" container>
-            {children}
-        </Grid>
-    )
-}
+const CenterWrapper:FC = ({children}) => (
+    <Grid className={styles.wrapper} justifyContent="center" container>
+        {children}
+    </Grid>
+)
+
 
 export default CenterWrapper
